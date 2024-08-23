84af04b400870061243b19018b17c2ba3f6f9da6
Tar1kDemir <t4r1kd3mir@gmail.com>
2024-08-23 12:18:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+import pygame
+import time
+
+pygame.init()
+pygame.mixer.init()
+sound = pygame.mixer.Sound("C:/deneme/deneme.mp3")
+def play_sound_after_delay():
+    time.sleep(0.5) 
+    sound.play()
+
+root = tk.Tk()
+root.overrideredirect(True)
+
+# m'2
+screen_width = root.winfo_screenwidth()
+screen_height = root.winfo_screenheight()
+window_width = 350
+window_height = 500
+x_position = screen_width - window_width - 10
+y_position = screen_height - window_height - 50
+root.geometry(f'{window_width}x{window_height}+{x_position}+{y_position}')
+
+root.attributes("-transparentcolor", "#676767")
+
+image = Image.open("C:/deneme/welcome.png")
+image = image.resize((350, 500), Image.LANCZOS) 
+photo = ImageTk.PhotoImage(image)
+
+label = tk.Label(root, image=photo, bg="white")
+label.pack()
+
+root.after(500, play_sound_after_delay)
+root.after(10000, root.destroy)  # t
+root.mainloop()
