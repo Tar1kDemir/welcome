1273feca4c8b2c66d5dbb0be3b3b483b8ab8959a
Tar1kDemir <t4r1kd3mir@gmail.com>
2024-08-23 12:24:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 pygame.init()
 pygame.mixer.init()
-sound = pygame.mixer.Sound("C:/deneme/deneme.mp3")
+sound = pygame.mixer.Sound("C:/welcome/voice.mp3") #ses yolu
 def play_sound_after_delay():
     time.sleep(0.5) 
     sound.play()
@@ -22,9 +22,9 @@ x_position = screen_width - window_width - 10
 y_position = screen_height - window_height - 50
 root.geometry(f'{window_width}x{window_height}+{x_position}+{y_position}')
 
-root.attributes("-transparentcolor", "#676767")
+root.attributes("-transparentcolor", "#676767") #arka planı silmek için benim kullandığım resmin arka planı #676767 idi kullandığınız resme göre ayarlayın
 
-image = Image.open("C:/deneme/welcome.png")
+image = Image.open("C:/welcome/welcome.png") #resmin yolu
 image = image.resize((350, 500), Image.LANCZOS) 
 photo = ImageTk.PhotoImage(image)
 
